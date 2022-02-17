baa2070d35fbacf4a0c782b1c142ccba51504ccc
MYY1106 <361598804@qq.com>
2022-02-17 15:32:09 +0800

    chore: window电脑配置

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,6 +4,22 @@ dotenv.config();
 
 console.log(process.env.APP_PORT);
 
-const { APP_PORT } = process.env;
+const {
+    APP_PORT,
+    MYSQL_HOST,
+    MYSQL_PORT,
+    MYSQL_DATABASE,
+    MYSQL_USER,
+    MYSQL_PASSWORD
+} = process.env;
 
-export { APP_PORT };
+const MYSQL_PORT_NUM = parseInt(MYSQL_PORT);
+
+export {
+    APP_PORT,
+    MYSQL_HOST,
+    MYSQL_PORT_NUM,
+    MYSQL_DATABASE,
+    MYSQL_USER,
+    MYSQL_PASSWORD
+};
