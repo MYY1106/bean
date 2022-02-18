7507004c8eafd15832c0bdafae756838a7ec9eb9
MYY1106 <361598804@qq.com>
2022-02-18 15:16:19 +0800

    chore: 部署服务器

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,20 @@
 import fs from 'fs'
 import { myKoa } from '../@types'
+console.log(1);
+console.log(1);
+
+console.log(1);
+
+
+
 
 const useRoutes = function () {
+    console.log(this);
     fs.readdirSync(__dirname).forEach(file => {
         if (file === 'index.ts') return;
         const router = require(`./${file}`).default;
-        (this as myKoa).use(router.routes());
-        (this as myKoa).use(router.allowedMethods());
+        (this as any).use(router.routes());
+        (this as any).use(router.allowedMethods());
     })
 }
 
